4df0194abb3c27ef50205bbe07ec3d66ad4054aa
samuraiht <aauioe@gmail.com>
2021-10-31 08:23:24 +0000

    ユーザー画面

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ window.onload = () => {
 	  console.log(data);
 	  const text = document.getElementById('response');
 	  switch(data.result) {
-	    case 0:
+	    case 0:// {"result":0,"count":4}
 	      text.textContent = data.count;
 	      break;
-	    case 1:
+	    case 1:// {"result":1,"count":0}
 	      text.textContent = '存在しない品目です';
 	      break;
-	    case 2:
+	    case 2:// {"result":2,"count":0}
 	      text.textContent = 'パラメーターが正しくありません';
 	      break;
-	    case 3:
+	    case 3:// {"result":3,"count":0}
 	    	text.textContent = 'データベース接続失敗';
 	  }
 	}
