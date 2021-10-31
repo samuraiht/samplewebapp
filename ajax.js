998d98cd50a6bca4a2963d94ed70d2f509d43e75
samuraiht <aauioe@gmail.com>
2021-10-31 08:20:32 +0000

    非同期通信JavaScriptプラグイン

--- ajax.js
+++ ajax.js
@@ -1,14 +1,18 @@
 async function fetchJSON(requestURL, getQuery = '', postQuery = '') {
+  const usePost = postQuery != '';// POSTかどうか
+
+// リクエストデータ
   const data = {
-    method: postQuery != '' ? 'POST' : 'GET',
-    headers: {'Content-Type': postQuery != '' ? 'application/x-www-form-urlencoded' : 'text/plain'},
+    method: usePost ? 'POST' : 'GET',
+    headers: {'Content-Type': usePost ? 'application/x-www-form-urlencoded' : 'text/plain'},
     mode: 'cors',
     cache: 'no-cache',
     credentials: 'same-origin',
     redirect: 'follow',
     referrerPolicy: 'no-referrer'
   };
-  if(postQuery != '') data.body = postQuery;
+  if(usePost) data.body = postQuery;
+
   const response = await fetch(requestURL + (getQuery != '' ? '?' + getQuery : ''), data);
 　return response.json();
 }
